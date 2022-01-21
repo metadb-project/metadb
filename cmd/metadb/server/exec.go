c6d6fc43d3bef00b206a0249e2b438a713f4bc1e
Nassib Nassar <nrn@etymon.com>
2022-01-21 13:57:13 -0500

    Turn off matcher debugging

--- cmd/metadb/server/exec.go
+++ cmd/metadb/server/exec.go
@@ -242,10 +242,10 @@ func execMergeData(c *command.Command, tx *sql.Tx, db sqlx.DB) error {
 	}
 	if ident {
 		if cf == "false" {
-			log.Trace("matched_cf")
+			// log.Trace("matcher cf")
 			return updateRowCF(c, tx, db, &t, id)
 		}
-		log.Trace("matched_ok")
+		// log.Trace("matcher ok")
 		return nil
 	}
 	exec := make([]string, 0)
@@ -376,7 +376,7 @@ func isCurrentIdentical(c *command.Command, tx *sql.Tx, db sqlx.DB, t *sqlx.Tabl
 			}
 		}
 	} else {
-		log.Trace("matcher: %s: row not found in database: %s", t, command.ColumnsString(c.Column))
+		// log.Trace("matcher: %s: row not found in database: %s", t, command.ColumnsString(c.Column))
 		return false, "", "", nil
 	}
 	for _, col := range c.Column {
@@ -391,18 +391,18 @@ func isCurrentIdentical(c *command.Command, tx *sql.Tx, db sqlx.DB, t *sqlx.Tabl
 			ddatas = *ddata
 		}
 		if (cdata == nil && ddata != nil) || (cdata != nil && ddata == nil) {
-			log.Trace("matcher: %s (%s): cdata(%v) != ddata(%v)", t, col.Name, cdata, ddata)
+			// log.Trace("matcher: %s (%s): cdata(%v) != ddata(%v)", t, col.Name, cdata, ddata)
 			return false, id, cf, nil
 		}
 		if cdata != nil && ddata != nil && cdatas != ddatas {
-			log.Trace("matcher: %s (%s): cdatas(%s) != ddatas(%s)", t, col.Name, cdatas, ddatas)
+			// log.Trace("matcher: %s (%s): cdatas(%s) != ddatas(%s)", t, col.Name, cdatas, ddatas)
 			return false, id, cf, nil
 		}
 		delete(attrs, col.Name)
 	}
 	for k, v := range attrs {
 		if v != nil {
-			log.Trace("matcher: %s (%s): database has extra value: %v", t, k, v)
+			// log.Trace("matcher: %s (%s): database has extra value: %v", t, k, v)
 			return false, id, cf, nil
 		}
 	}
