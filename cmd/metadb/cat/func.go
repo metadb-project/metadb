5ee4fecbc20d7c9efa8531fec664b0b2ad2fe421
Nassib Nassar <nrn@etymon.com>
2022-10-23 17:11:50 -0400

    Add functions to list authorizations, origins, and sources

--- cmd/metadb/cat/func.go
+++ cmd/metadb/cat/func.go
@@ -5,18 +5,69 @@ import (
 	"fmt"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/metadb-project/metadb/cmd/metadb/dbx"
+	"github.com/metadb-project/metadb/cmd/metadb/log"
 	"github.com/metadb-project/metadb/cmd/metadb/util"
 )
 
-// TODO move to func and check existence and value of metadb_version() before creating;
-// but note this requires user permissions to be fixed
-
 var functionDefs = [][]string{
-	{"metadb_version()",
-		"CREATE OR REPLACE FUNCTION public.metadb_version() RETURNS text\n" +
-			"    AS $$ SELECT 'Metadb " + util.MetadbVersion + "' $$\n" +
-			"    LANGUAGE SQL;"},
+	{"metadb_version()", `
+CREATE FUNCTION public.metadb_version() RETURNS text
+    AS $$ SELECT 'Metadb ` + util.MetadbVersion + `' $$
+    LANGUAGE SQL`},
+	{"list_authorizations()", `
+CREATE FUNCTION public.list_authorizations()
+    RETURNS TABLE (
+        username text,
+        note text
+    )
+    AS $$
+    SELECT username,
+           CASE WHEN (NOT dbupdated) THEN 'pending restart'
+                WHEN (tables='.*' AND dbupdated) THEN 'authorized'
+                ELSE 'not authorized'
+           END note
+    FROM metadb.auth
+    $$ LANGUAGE SQL`},
+	{"list_data_sources()", `
+CREATE FUNCTION public.list_data_sources()
+    RETURNS TABLE (
+        name text,
+        brokers text,
+        security text,
+        topics text,
+        consumergroup text,
+        schemapassfilter text,
+        trimschemaprefix text,
+        addschemaprefix text,
+        module text
+    )
+    AS $$
+    SELECT name,
+           brokers,
+           security,
+           topics,
+           consumergroup,
+           schemapassfilter,
+           trimschemaprefix,
+           addschemaprefix,
+           module
+    FROM metadb.source
+    $$ LANGUAGE SQL`},
+	{"list_data_origins()", `
+CREATE FUNCTION public.list_data_origins()
+    RETURNS TABLE (
+        name text
+    )
+    AS $$
+    SELECT name
+    FROM metadb.origin
+    $$ LANGUAGE SQL`},
 }
+
+/*
+  list_status_data_sources()
+*/
 
 func CreateAllFunctions(dcsuper, dc *pgx.Conn, systemuser string) error {
 	q := "GRANT CREATE, USAGE ON SCHEMA public TO " + systemuser
@@ -31,15 +82,9 @@ func CreateAllFunctions(dcsuper, dc *pgx.Conn, systemuser string) error {
 	}
 
 	for _, f := range functionDefs {
-		fname := f[0]
-		fdef := f[1]
-		_, err = dc.Exec(context.TODO(), fdef)
+		err := createFunction(dc, f[0], f[1], users)
 		if err != nil {
-			return fmt.Errorf("creating function: %s: %v", fname, err)
-		}
-		for _, u := range users {
-			q := "GRANT EXECUTE ON FUNCTION public." + fname + " TO " + u
-			_, _ = dc.Exec(context.TODO(), q)
+			return err
 		}
 	}
 
@@ -48,5 +93,44 @@ func CreateAllFunctions(dcsuper, dc *pgx.Conn, systemuser string) error {
 		_, _ = dcsuper.Exec(context.TODO(), q)
 	}
 
+	log.Trace("created database functions")
+	return nil
+}
+
+func createFunction(dc *pgx.Conn, fname, fdef string, users []string) error {
+	tx, err := dc.Begin(context.TODO())
+	if err != nil {
+		return fmt.Errorf("starting transaction for function: %s: %v", fname, err)
+	}
+	defer dbx.Rollback(tx)
+
+	q := "DROP FUNCTION IF EXISTS public." + fname
+	_, err = tx.Exec(context.TODO(), q)
+	if err != nil {
+		return fmt.Errorf("dropping function: %s: %v", fname, err)
+	}
+
+	_, err = tx.Exec(context.TODO(), fdef)
+	if err != nil {
+		return fmt.Errorf("creating function: %s: %v", fname, err)
+	}
+
+	q = "REVOKE EXECUTE ON FUNCTION public." + fname + " FROM public;"
+	_, err = tx.Exec(context.TODO(), q)
+	if err != nil {
+		return fmt.Errorf("revoking public access to function: %s: %v", fname, err)
+	}
+
+	if fname == "metadb_version()" {
+		for _, u := range users {
+			q := "GRANT EXECUTE ON FUNCTION public." + fname + " TO " + u
+			_, _ = tx.Exec(context.TODO(), q)
+		}
+	}
+
+	err = tx.Commit(context.TODO())
+	if err != nil {
+		return err
+	}
 	return nil
 }
