d379aaa4d89ebc73d02a8cd6c2718b21b388cc3a
Nassib Nassar <nrn@etymon.com>
2023-02-02 11:49:44 -0500

    Update external SQL tag

--- cmd/metadb/server/server.go
+++ cmd/metadb/server/server.go
@@ -320,7 +320,7 @@ func checkTimeDailyMaintenance(datadir string, db dbx.DB, cat *catalog.Catalog, 
 		for {
 			tries++
 			url := "https://github.com/folio-org/folio-analytics.git"
-			tag := "v1.5.0"
+			tag := "v1.5.1"
 			path := "sql_metadb/derived_tables"
 			schema := "folio_derived"
 			if err := runsql.RunSQL(datadir, cat, db, url, tag, path, schema); err != nil {
