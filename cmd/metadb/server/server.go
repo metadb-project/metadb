ea59ad6b21857a9657e635b63bad13acf69bda6b
Nassib Nassar <nrn@etymon.com>
2023-04-26 13:27:44 -0400

    Update reshare tag to 20230426170621

--- cmd/metadb/server/server.go
+++ cmd/metadb/server/server.go
@@ -330,7 +330,7 @@ func checkTimeDailyMaintenance(datadir string, db dbx.DB, cat *catalog.Catalog, 
 		for {
 			tries++
 			url := "https://github.com/openlibraryenvironment/reshare-analytics.git"
-			tag := "v0.2.0-beta7"
+			tag := "20230426170621"
 			path := "sql/derived_tables"
 			schema := "reshare_derived"
 			if err = runsql.RunSQL(datadir, cat, db, url, tag, path, schema); err != nil {
