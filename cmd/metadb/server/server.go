fb629a2f76ee989ded9ded09aebd885391082362
Nassib Nassar <nrn@etymon.com>
2023-05-21 19:17:00 -0400

    runsql: update folio tag to 20230521231434

--- cmd/metadb/server/server.go
+++ cmd/metadb/server/server.go
@@ -325,7 +325,7 @@ func checkTimeDailyMaintenance(datadir string, db dbx.DB, dc *pgx.Conn, cat *cat
 		for {
 			tries++
 			url := "https://github.com/folio-org/folio-analytics.git"
-			tag := "v1.6.0"
+			tag := "20230521231434"
 			path := "sql_metadb/derived_tables"
 			schema := "folio_derived"
 			if err = runsql.RunSQL(datadir, cat, db, url, tag, path, schema); err != nil {
