2be471c7b2b4e6d05faa90e8c1ef01cddfd04a15
Nassib Nassar <nrn@etymon.com>
2023-12-05 14:04:07 -0500

    Update folio tag to v1.7.1

--- cmd/metadb/server/server.go
+++ cmd/metadb/server/server.go
@@ -294,7 +294,7 @@ func checkTimeDailyMaintenance(datadir string, db dbx.DB, dp *pgxpool.Pool, cat 
 		for {
 			tries++
 			url := "https://github.com/folio-org/folio-analytics.git"
-			ref := "refs/tags/v1.7.0"
+			ref := "refs/tags/v1.7.1"
 			path := "sql_metadb/derived_tables"
 			schema := "folio_derived"
 			if err = runsql.RunSQL(datadir, cat, db, url, ref, path, schema, source); err != nil {
