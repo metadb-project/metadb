aa9b9899e6a9cbf6fbdc61b4f73b1663d0ea656e
Nassib Nassar <nrn@etymon.com>
2025-06-05 23:22:05 -0400

    improve validating data mapping arguments

--- cmd/metadb/libpq/createmap.go
+++ cmd/metadb/libpq/createmap.go
@@ -12,12 +12,36 @@ import (
 	"github.com/metadb-project/metadb/cmd/metadb/dbx"
 )
 
+var schemaTableRegexp = regexp.MustCompile(`^[_a-z][0-9_a-z]*[.][_a-z][0-9_a-z]*[_][_]$`)
+var columnRegexp = regexp.MustCompile(`^[_a-z][0-9_a-z]*$`)
 var identifierRegexp = regexp.MustCompile(`^[a-z][0-9a-z]*$`)
 
 func createDataMapping(conn net.Conn, node *ast.CreateDataMappingStmt, cat *catalog.Catalog) error {
 	// The only mapping type currently supported is json.
 	if node.TypeName != "json" {
 		return fmt.Errorf("mapping type %q not supported", node.TypeName)
+	}
+
+	// Validate schema.table name.
+	if node.TableName == "" || !schemaTableRegexp.MatchString(node.TableName) {
+		return fmt.Errorf("table name %q is invalid", node.TableName)
+	}
+	// Parse the schema.table name.
+	table, err := dbx.ParseTable(node.TableName[0 : len(node.TableName)-2])
+	if err != nil {
+		return fmt.Errorf("%q is not a valid table name", node.TableName)
+	}
+	// Validate table name length.
+	if len(table.Schema) > 63 || len(table.Table) > 63 {
+		return fmt.Errorf("%q is not a valid table name", node.TableName)
+	}
+
+	// Validate column name.
+	if node.ColumnName == "" || !columnRegexp.MatchString(node.ColumnName) {
+		return fmt.Errorf("column name %q is invalid", node.ColumnName)
+	}
+	if len(node.ColumnName) > 63 {
+		return fmt.Errorf("%q is not a valid column name", node.ColumnName)
 	}
 
 	// Validate path.
@@ -35,23 +59,9 @@ func createDataMapping(conn net.Conn, node *ast.CreateDataMappingStmt, cat *cata
 	if node.TargetIdentifier == "" || !identifierRegexp.MatchString(node.TargetIdentifier) {
 		return fmt.Errorf("target identifier %q is invalid", node.TargetIdentifier)
 	}
-
-	// Ensure the table name is a main table, and parse it.
-	if !strings.HasSuffix(node.TableName, "__") {
-		return fmt.Errorf("%q is not a main table name", node.TableName)
-	}
-	table, err := dbx.ParseTable(node.TableName[0 : len(node.TableName)-2])
-	if err != nil {
-		return fmt.Errorf("%q is not a valid table name", node.TableName)
-	}
-	// Validate table name.
-	if len(table.Schema) > 63 || len(table.Table) > 63 {
-		return fmt.Errorf("%q is not a valid table name", node.TableName)
-	}
-
-	// Validate column name.
-	if len(node.ColumnName) > 63 {
-		return fmt.Errorf("%q is not a valid column name", node.ColumnName)
+	const maxTargetIdentifierLen = 16
+	if len(node.TargetIdentifier) > maxTargetIdentifierLen {
+		return fmt.Errorf("target identifier %q is too long (maximum length %d characters)", node.TargetIdentifier, maxTargetIdentifierLen)
 	}
 
 	if err := cat.DefineJSONMapping(table.Schema, table.Table, node.ColumnName, node.Path, node.TargetIdentifier); err != nil {
